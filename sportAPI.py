86cc9a448e5e2bfc840cc20a160690f5958cc432
darekpe79 <94934717+darekpe79@users.noreply.github.com>
2021-11-24 20:59:09 +0100

    Add files via upload

--- sportAPI.py
+++ sportAPI.py
@@ -1,11 +1,12 @@
 
-
+#%%
 import requests
 import pandas as pd
 import json
 import streamlit as st
 import plotly.express as px
 from PIL import Image
+
 
 with open('sample.json', 'r') as openfile:
 
@@ -52,7 +53,113 @@ golasagainstminutes=json_object['response']['goals']['against']['minute']
 pd4=pd.DataFrame.from_dict(golasagainstminutes)
 df_goalsagainstminutes = pd4.rename(index={'total': 'total_against', 'percentage':'percentage_against'})
 result = df_goalsforminutes.append(df_goalsagainstminutes)
+#LEAGUE TABLE
+#
+with open('league_stand.json', 'r') as openfile:
+
+    # Reading from json file
+    json_object = json.load(openfile)
+    
+#print(json.dumps(json_object, indent=4))
+proba=json_object['response'][0]['league']['standings']
+rank_list=[]
+team_id_list=[]
+team_name_list=[]
+points_list=[]
+goalsDiff_list=[]
+form_list=[]
+status_list=[]
+description_list=[]
+allgameplayed_list=[]
+allgamewin_list=[]
+allgamedraw_list=[]
+allgamelose_list=[]
+allgamegoalfor_list=[]
+allgamegoalagainst_list=[]
+home_played_list=[]
+homewin_list=[]
+homegamedraw_list=[]
+homelose_list=[]
+homegamegoalfor_list=[]
+homegamegoalagainst_list=[]
+away_played_list=[]
+awaywin_list=[]
+awaygamedraw_list=[]
+awaylose_list=[]
+awaygamegoalfor_list=[]
+awaygamegoalagainst_list=[]
+for teams in proba:
+    #print(teams)
+    for team in teams:
+        rank=team['rank']
+        team_id=team['team']['id']
+        team_name=team['team']['name']
+        points=team['points']
+        goalsDiff=team['goalsDiff']
+        form=team['form']
+        status=team['status']
+        description=team['description']
+        allgameplayed=team['all']['played']
+        allgamewin=team['all']['win']
+        allgamedraw=team['all']['draw']
+        allgamelose=team['all']['lose']
+        allgamegoalfor=team['all']['goals']['for']
+        allgamegoalagainst=team['all']['goals']['against']
+        home_played=team['home']['played']
+        homewin=team['home']['win']
+        homegamedraw=team['home']['draw']
+        homelose=team['home']['lose']
+        homegamegoalfor=team['home']['goals']['for']
+        homegamegoalagainst=team['home']['goals']['against']
+        away_played=team['away']['played']
+        awaywin=team['away']['win']
+        awaygamedraw=team['away']['draw']
+        awaylose=team['away']['lose']
+        awaygamegoalfor=team['away']['goals']['for']
+        awaygamegoalagainst=team['away']['goals']['against']
+        rank_list.append(rank)
+        team_id_list.append(team_id)
+        team_name_list.append(team_name)
+        points_list.append(points)
+        goalsDiff_list.append(goalsDiff)
+        form_list.append(form)
+        status_list.append(status)
+        description_list.append(description)
+        allgameplayed_list.append(allgameplayed)
+        allgamewin_list.append(allgamewin)
+        allgamedraw_list.append(allgamedraw)
+        allgamelose_list.append(allgamelose)
+        allgamegoalfor_list.append(allgamegoalfor)
+        allgamegoalagainst_list.append(allgamegoalagainst)
+        home_played_list.append(home_played)
+        homewin_list.append(homewin)
+        homegamedraw_list.append(homegamedraw)
+        homelose_list.append(homelose)
+        homegamegoalfor_list.append(homegamegoalfor)
+        homegamegoalagainst_list.append(homegamegoalagainst)
+        away_played_list.append(away_played)
+        awaywin_list.append(awaywin)
+        awaygamedraw_list.append(awaygamedraw)
+        awaylose_list.append(awaylose)
+        awaygamegoalfor_list.append(awaygamegoalfor)
+        awaygamegoalagainst_list.append(awaygamegoalagainst)
+list_of_tuples_all = list(zip(rank_list,
+team_id_list,
+team_name_list,
+form_list,
+status_list,
+description_list,
+allgameplayed_list,
+allgamewin_list,
+allgamedraw_list,
+allgamelose_list,
+goalsDiff_list,
+allgamegoalfor_list,
+allgamegoalagainst_list,points_list))
+dfleagueall = pd.DataFrame(list_of_tuples_all, columns = ['rank', 'team_id','team_name','form','status','description','played','win','draw','lose','goals_diff','goals_for','goals_against','points']) 
+dfleagueall=dfleagueall.set_index('rank')
 #%% streamlit run sportAPI.py
+
 st.set_page_config(page_title='ŁKS STATISTICS')
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
@@ -66,7 +173,7 @@ st.markdown(f""" <style>
         padding-left: {padding}rem;
         padding-bottom: {padding}rem;
     }} </style> """, unsafe_allow_html=True)
-
+st.dataframe(dfleagueall)
 st.header('ŁKS  Form: '+forma)
 st.header('Fixtures')
 #pd1['index1'] = pd1.index
@@ -106,6 +213,6 @@ st.header('Goals Against, Minutes')
 st.table(df_goalsagainstminutes.style.format({'0-15': '{:.1f}', '16-30': '{:.1f}', '31-45': '{:.1f}','46-60':'{:.1f}','61-75':'{:.1f}','76-90':'{:.1f}', '91-105':'{:.1f}','106-120':'{:.1f}'}))
 #st.dataframe(df_goalsforminutes)
 st.header('Our league')
-st.dataframe(our_league_df)
+st.table(our_league_df)
 
 ##df['col'] = df['col'].str.rstrip('%').astype('float') / 100.0
